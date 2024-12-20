b4ce52609ac81b07a2ead625f4dff53bd79fea4d
Nhat Vo <nhat.vo@itdragons.com>
2024-12-20 18:29:45 +0700

    Update home page

--- wp-content/themes/main/page-home.php
+++ wp-content/themes/main/page-home.php
@@ -25,6 +25,76 @@
         </div>
     </section>
 
+    <section class="camping-gallery">
+        <div class="camping-horizontal-gallery">
+            <div class="camping-image-list">
+                <img src="<?php echo theme_uri()?>/assets/images/gallery-01.jpg" alt="Image 1">
+                <img src="<?php echo theme_uri()?>/assets/images/gallery-02.jpg" alt="Image 1">
+                <img src="<?php echo theme_uri()?>/assets/images/gallery-03.jpg" alt="Image 1">
+                <img src="<?php echo theme_uri()?>/assets/images/gallery-03.jpg" alt="Image 1">
+                <img src="<?php echo theme_uri()?>/assets/images/gallery-03.jpg" alt="Image 1">
+                <img src="<?php echo theme_uri()?>/assets/images/gallery-03.jpg" alt="Image 1">
+            </div>
+        </div>
+    </section>
+
+
+    <section class="camping-memories">
+        <div class="container">
+            <div class="camping-memories-top">
+                <h5>Trại hè 2024</h5>
+                <h1>bi ve - hồi ức tuổi thơ</h1>
+                <p>Ký ức tuổi thơ là những ngày hè nắng chang chang, lũ con nít chúng mình túm năm tụm ba sau trường,
+                    háo hức dõi theo những viên bi ve đủ màu, lấp lánh như những vì sao....
+                </p>
+            </div>
+            <div class="camping-memories-center">
+                <div class="left">
+                    <img src="<?php echo theme_uri()?>/assets/images/memories-01.jpg">
+                </div>
+                <div class="right">
+                    <p>Ngày ấy, cứ chiều chiều sau khi tan học, đám trẻ lại kéo nhau ra khoảng sân rộng trong làng,
+                        tụ tập lập hội bắn bi. Đứa nào cũng có một sưu tập bi đủ loại, đủ kích cỡ, đủ sắc màu, như một
+                        kho báu quý giá mà không gì có thể đổi lấy được.
+                    </p>
+                    <img class="image-position" src="<?php echo theme_uri()?>/assets/images/memories-04.png ">
+                </div>
+            </div>
+            <div class="camping-memories-bot">
+                <div class="left">
+                    <p>
+                        Được lấy cảm hứng từ trò bắn bi – trò chơi vui thú một thời tuổi thơ suốt những năm 7x, 8x, 9x,
+                        sản phẩm Bánh Trung thu 2024 của Cộng Cà Phê có thiết kế bao bì và hộp quà bi ve tặng kèm,
+                        chứa đựng muôn vàn kỷ niệm hồn nhiên thời thơ bé.
+                    </p>
+                    <br>
+                    <p>
+                        Hy vọng một chút quà tặng bi ve nhỏ xinh trong mỗi Hộp Bánh Trung Thu của Cộng sẽ mang lại
+                        cho bạn và gia đình những niềm vui trong trẻo, làm sống lại hồi ức tuổi thơ thân thương trong
+                        mỗi chúng ta.
+                    </p>
+                </div>
+                <div class="right">
+                    <img src="<?php echo theme_uri()?>/assets/images/memories-03.jpg">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="promo-section background-scroll" style="background-image: url('<?php echo theme_uri()?>/assets/images/what-have.jpg');">
+        <div class="promo-title">
+            <h1>TRẠI HÈ NĂM NAY</h1>
+            <h2>TOMOE CÓ GÌ?</h2>
+        </div>
+    </section>
+
+    <section class="camping-current">
+        <div class="camping-current-title">
+            <h5>Trại hè 2024</h5>
+            <h1>Trại hè năm nay có</h1>
+        </div>
+    </section>
+
     <section class="thanks">
         <div class="thanks-img" style="background-image: url('<?php echo theme_uri()?>/assets/images/footer-img.png');">
             <div class="container">

6a365c20ffb6c138d70be30771babe4bf8cad33c
nhatvo <nhat.vo@itdragons.com>
2024-12-20 15:19:24 +0700

    Create camping info and thanks section responsive

--- wp-content/themes/main/page-home.php
+++ wp-content/themes/main/page-home.php
@@ -1,1 +1,43 @@
-<?php echo 'Hello World!'; ?>
+<?php get_header();?>
+
+    <section class="banner" style="background-image: url('<?php echo theme_uri()?>/assets/images/banner.jpg');">
+        <div class="container banner-wrap">
+        </div>
+    </section>
+
+    <section class="camping-info">
+        <div class="container">
+            <div class="camping-info-content">
+                <div class="camping-info-left">
+                    <h5>Trại hè 2024</h5>
+                    <h1>Giới thiệu</h1>
+                </div>
+                <div class="camping-info-right">
+                    <div class="camping-info-description">
+                        <p>Dịp Trăng rằm năm nay, Cộng chở bạn về tuổi thơ những ngày thương nhớ cùng Hộp Bánh Trung Thu, mở ra là cả gia tài nhỏ - đầy ắp những viên bi ve lấp lánh.</p>
+
+                        <p>Những hồi ức sống động, thuần khiết, đầy màu sắc, lăn tròn nhẹ nhàng như thước phim cuộn lại.</p>
+
+                        <p>Mùa đoàn viên này, Cộng mong được đồng hành cùng bạn và gia đình, Chạm về một thời thơ ấu Trong veo, Chạm từ hiện tại về quá khứ, Chạm để thêm gắn kết, Chạm để được yêu thương.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="thanks">
+        <div class="thanks-img" style="background-image: url('<?php echo theme_uri()?>/assets/images/footer-img.png');">
+            <div class="container">
+                <div class="thanks-content">
+                    <h5>Trại hè 2024</h5>
+                    <h1>chúc bạn có một trại hè vui vẻ!</h1>
+                    <p>
+                        Cảm ơn bạn đã quan tâm đến bộ sưu tập Bánh Trung Thu 2024 của Cộng Cà Phê.
+                        Năm nay đủ bánh, đủ trà, có cả chút quà hoài niệm, mùa Trăng thêm ngọt lành, đong trọn đầy niềm vui!
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<?php get_footer();?>
